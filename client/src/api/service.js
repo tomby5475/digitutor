098b14876fd4f04deb292e004db021af0abe47b9
Nataly <pushkareva.nataly@gmail.com>
2022-05-19 18:33:41 +0200

    change BaseUrl

--- client/src/api/service.js
+++ client/src/api/service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: "http://localhost:5005/api"
+  baseURL: "https://digitutor.herokuapp.com/"
 });
 
 const errorHandler = err => {
